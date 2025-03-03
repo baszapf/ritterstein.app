5fb41f39cb7b8924ed22348af9e6c0522921e5d5
Bas Zapf <bas@digitalnomad.de>
2025-03-03 22:30:19 +0100

    added vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/ritterstein.app/",
+  base: "/ritterstein.app/", // Name deines Repos (WICHTIG!)
   plugins: [react()],
 });

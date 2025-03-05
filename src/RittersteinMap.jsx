e610644b3aa4805852a8ec84db3bce237ef990e8
Bas Zapf <bas@digitalnomad.de>
2025-03-05 20:08:49 +0100

    new markers

--- src/RittersteinMap.jsx
+++ src/RittersteinMap.jsx
@@ -9,6 +9,31 @@ function RittersteinMap() {
   const [userPosition, setUserPosition] = useState(null);
   const [closestRittersteine, setClosestRittersteine] = useState([]);
 
+  const userIcon = new L.DivIcon({
+    html: `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+      <rect x="9" y="0" width="6" height="24" fill="red"/>
+      <rect x="0" y="9" width="24" height="6" fill="red"/>
+    </svg>`,
+    className: '', // Entfernt Leaflet-Standard-Styling
+    iconSize: [24, 24],
+    iconAnchor: [12, 12],
+  });
+  export default userIcon;
+
+  const rittersteinIcon = new L.DivIcon({
+    html: `<svg xmlns="http://www.w3.org/2000/svg" width="32" height="48" viewBox="0 0 24 36">
+      <path fill="red" stroke="black" stroke-width="2" d="M12 1C6.48 1 2 5.48 2 11c0 7.18 10 21 10 21s10-13.82 10-21c0-5.52-4.48-10-10-10z"/>
+      <circle cx="12" cy="11" r="4" fill="white" stroke="black" stroke-width="2"/>
+    </svg>`,
+    className: '',
+    iconSize: [32, 48],
+    iconAnchor: [16, 48],
+    popupAnchor: [0, -48]
+  });
+  export default rittersteinIcon;
+
+  
+
   useEffect(() => {
     // Daten für Rittersteine laden
     fetch('rittersteine.json')
@@ -57,25 +82,18 @@ function RittersteinMap() {
 
         {/* Benutzerposition */}
         {userPosition && (
-          <Marker position={[userPosition.lat, userPosition.lon]}>
+          <Marker position={[userPosition.lat, userPosition.lon]} icon={userIcon}>
             <Popup>Deine Position</Popup>
           </Marker>
         )}
 
         {/* Rittersteine */}
         {rittersteine.map((stein, index) => (
-          
-
 
           <Marker
             key={index}
             position={[stein.lat, stein.lon]}
-            icon={L.icon({
-              iconUrl: 'https://unpkg.com/leaflet/dist/images/marker-icon.png', // Standard-Leaflet-Icon
-              iconSize: [25, 41], // Größe des Icons
-              iconAnchor: [12, 41], // Ankerpunkt für das Icon
-              popupAnchor: [1, -34], // Position des Popups
-            })}
+            icon={rittersteinIcon}
           >
             <Popup>{stein.name}</Popup>
           </Marker>

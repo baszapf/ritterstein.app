48f290eb452b9fb1d41d0891e8a76e450cd9b6ba
Bas Zapf <bas@digitalnomad.de>
2025-03-08 11:24:12 +0100

    work in progress

--- src/RittersteinMap.jsx
+++ src/RittersteinMap.jsx
@@ -1,9 +1,9 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import { MapContainer, TileLayer, Marker, Popup, Polyline, useMap } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
 import { getDistance } from 'geolib';
-import { rittersteinIcon, closestRittersteinIcon, userIcon } from './assets/icons/icons';s
+import { rittersteinIcon, closestRittersteinIcon, userIcon } from './assets/icons/icons';
 
 function MapCenter({ position }) {
   const map = useMap();
@@ -20,8 +20,6 @@ function RittersteinMap() {
   const [rittersteine, setRittersteine] = useState([]);
   const [userPosition, setUserPosition] = useState(null);
   const [closestRittersteine, setClosestRittersteine] = useState([]);
-  const mapRef = useRef(null);
-  const initialCentered = useRef(false);
 
   useEffect(() => {
     fetch('rittersteine.json')
@@ -31,17 +29,10 @@ function RittersteinMap() {
     const updateUserPosition = () => {
       if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition((position) => {
-          const newPosition = {
+          setUserPosition({
             lat: position.coords.latitude,
             lon: position.coords.longitude,
-          };
-          setUserPosition(newPosition);
-
-          // Karte nur einmal initial zentrieren
-          if (!initialCentered.current && mapRef.current) {
-            mapRef.current.setView([newPosition.lat, newPosition.lon]);
-            initialCentered.current = true;
-          }
+          });
         });
       }
     };
@@ -67,19 +58,12 @@ function RittersteinMap() {
     }
   }, [userPosition, rittersteine]);
 
-  const centerMapOnUser = () => {
-    if (mapRef.current && userPosition) {
-      mapRef.current.setView([userPosition.lat, userPosition.lon]);
-    }
-  };
-
   return (
     <div>
       <MapContainer
         center={userPosition ? [userPosition.lat, userPosition.lon] : [49.44, 7.76]}
         zoom={10}
         style={{ height: '500px', width: '100%' }}
-        whenCreated={(map) => (mapRef.current = map)}
       >
         <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
 
@@ -121,24 +105,6 @@ function RittersteinMap() {
             </Polyline>
           ))}
       </MapContainer>
-
-      {/* Button zum Zentrieren auf Nutzerposition */}
-      <button
-        onClick={centerMapOnUser}
-        style={{
-          position: 'absolute',
-          top: '10px',
-          right: '10px',
-          zIndex: 1000,
-          padding: '10px',
-          background: 'white',
-          border: '1px solid black',
-          borderRadius: '5px',
-          cursor: 'pointer',
-        }}
-      >
-        Meine Position
-      </button>
     </div>
   );
 }
